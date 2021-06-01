7daa50f7413342811a947b1d1364b30b0b678266
tpham1467 <tpham1467@gmail.com>
2021-06-01 20:14:29 +0700

    lam do hoa

--- pbl.cpp
+++ pbl.cpp
@@ -1,29 +1,38 @@
+#include <graphics.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include <conio.h>
-#include <windows.h>
+#include<time.h>
+#include <iostream>
+using namespace std;
+#include<windows.h>
 #define MAX 100
+#define Length 1300
+#define Width 1000
 bool check_find = false;
 int location_to_delete;
 int total_number_of_students = 0;
 bool check_innit = false;
 bool check_sort = false;
 bool check_provide_student_code = false;
+FILE *p;
 struct first_and_last_name{
-    char first_name[15];
-    char last_name[10];
+    char first_name[25];
+    char last_name[8];
 
 };
 struct student{    
     first_and_last_name name;
-    char gender[5];
+    char gender[4];
     char Date_of_birth[11];
     char address[50];
-    char student_code[10];
-    char email[23];
+    char student_code[8];
+    char email[15];
 };
 typedef  student SV; 
+void init();
+int mainloop(SV student[]);
 void add_students_step1( SV student[], int students_need_add ); 
 void add_students_step2( SV &sv ); 
 void quickSort_first_name_step2( SV student[] , int low , int high );  
@@ -42,35 +51,81 @@ void print_list_step1( SV student[] );
 void print_list_step2( SV sv );
 bool check_data( char data[] );  
 void output_file( SV student[] );
+void Read_in_file(SV student[]);
+void Read_information(SV &sv);
+void Filter_the_data_name(SV &sv,char name[]);
+void Filter_the_data_address(SV &sv,char address[]);
 // method main
 int main(){
 	SV student[MAX];
-	system("color 01");
-    printf("\n\n                                    PBL 1: DO AN LAP TRINH TINH TOAN\n");
-    printf("\n\n                            De Tai: Xay Dung Chuong Trinh Quan Li Sinh Vien \n");
-    printf("\n                     Sinh Vien Thuc Hien:                       Giao Vien Huong Dan:\n");
-    printf("                     Sinh Vien 1: Pham Van Tien Truong          Nguyen THi Minh Hy   \n");
-    printf("                     Sinh Vien 2:Pham Cong Huy\n");
-    getch();
-    while(true){
-    system("color 0f");
-    system("cls");
-    printf("\n\n\n");
-    printf("                            ------------------------------------------------\n");
-    printf("                           |               Moi Chon Yeu Cau:                |\n");
-    printf("                           |    1: Nhap Sinh Vien                           |\n");
-    printf("                           |    2: Sap Xep Sinh Vien                        |\n");
-    printf("                           |    3: Xoa Sinh Vien Theo Ten Hoac Ma Sinh Vien |\n");
-    printf("                           |    4: Tim Sinh Vien Theo Ten Hoac Ma Sinh Vien |\n");
-    printf("                           |    5: Cap Ma So Sinh Vien                      |\n");
-    printf("                           |    6: Cap Email                                |\n");
-    printf("                           |    7: In Danh Sach                             |\n");
-    printf("                           |    0: Thoat Chuong Trinh                       |\n");
-    printf("                           |                                                |\n");
-    printf("                            ------------------------------------------------\n\n\n"); 
-    printf("                           Vui Long Nhap Yeu Cau...");
+	init();
+    mainloop(student);
+	
+   
+}
+void init(){
+	initwindow (Length,Width);
+	settextstyle(2,0,7);
+	setbkcolor(2);
+	line(50,45,1000,45);
+	outtextxy(400,50,"PBL 1: DO AN LAP TRINH TINH TOAN");
+	setcolor(7);
+	line(50,350,1000,350);
+	line(50,350,50,45);
+	line(1000,350,1000,45);
+	outtextxy(350,100,"De Tai: Xay Dung Chuong Trinh Quan Li Sinh Vien");
+	setbkcolor(0);
+	outtextxy(100,150,"Sinh Vien Thuc Hien:                                         Giao Vien Huong Dan:");
+	outtextxy(100,200,">>Pham Cong Huy");
+	outtextxy(100,250,">>Pham Van Tien Truong");
+	outtextxy(430,200,">>Nguyen THi Minh Hy");
+	line(47,45,47,353);
+	line(48,353,1000,353);
+	delay(3000); 
+	cleardevice();
+	
+
+}
+int mainloop(SV student[]){
+	int x = 0;
+	char *s = new char[5];
+	while (x <= 100){
+		cleardevice();
+		setcolor (10);settextstyle(4,0,5);outtextxy (60,200,"   Loading...");
+		sprintf (s,"%d",x);
+		setcolor (4);
+		outtextxy (550,200,strcat(s,"%"));
+		if (x == 100)
+			delay (500);
+		delay (20);
+		x++;
+	}
+	cleardevice();
+	while(true){
+		setcolor (15);
+		settextstyle(2,0,7);
+		line(400,50,850,50);
+		line(850,50,850,500);
+		line(400,500,850,500);
+		line(400,50,400,500);
+		line(396,46,854,46);
+		line(854,46,854,504);
+		line(396,504,854,504);
+		line(396,46,396,504);
+		line(400,80,850,80);
+		outtextxy(430,52,"Moi Chon Yeu Cau:");
+		outtextxy(410,82,"1: Nhap Sinh Vien");
+		outtextxy(410,112,"2: Doc Danh sach Sinh Vien Tu File");
+		outtextxy(410,142,"3: Sap Xep Sinh Vien|");
+		outtextxy(410,172,"4: Xoa Sinh Vien Theo Ten Hoac Ma Sinh Vien");
+		outtextxy(410,202,"5: Tim Sinh Vien Theo Ten Hoac Ma Sinh Vien");
+		outtextxy(410,232,"6: Cap Ma So Sinh Vien");
+		outtextxy(410,262,"7: Cap Email");
+		outtextxy(410,292,"8: In Danh Sach");
+		outtextxy(410,322,"0: Thoat Chuong Trinh");
+		outtextxy(450,520,"Vui Long Nhap Yeu Cau...");
     int key; // request code  
-    scanf("%d",&key);
+    //scanf("%d",&key);
     printf("\n");
     switch(key){
     	case 1: {
@@ -93,13 +148,22 @@ int main(){
             check_provide_student_code=false;
 			break;
 		}
-    	case 2: {		
+        case 2: {
+        	check_innit=true;
+            Read_in_file(student);
+            check_sort = false;
+            check_provide_student_code=false;
+            getch();
+            break;
+        }
+    	case 3: {		
             if( check_innit ){
             	    system("cls");
             	    printf("\n\n                                Ban Da Chon Sap Xep Danh Sach Sinh Vien\n");
                     check_sort = true;
                     quickSort_last_name_step1( student , 0 , total_number_of_students-1 );
-                    quickSort_first_name_step1( student );
+               
+			        quickSort_first_name_step1( student );
                     printf("\n\n\n                                Ban Da Sap Xep!");
                     getch();
                     break;
@@ -114,7 +178,7 @@ int main(){
             }
       
 	    }
-    	case 3: {
+    	case 4: {
             
             if( check_innit ){
             	    system("cls");
@@ -133,7 +197,7 @@ int main(){
 
             }
         }
-    	case 4: {
+    	case 5: {
             if( check_innit ){
             	system("cls");
             	system("color 06");
@@ -151,7 +215,7 @@ int main(){
 
             }
         }
-    	case 5: {
+    	case 6: {
             if( check_sort ){
             	system("cls");
             	system("color 06");
@@ -170,7 +234,7 @@ int main(){
 
             }
 		}
-    	case 6: {
+    	case 7: {
             if( check_provide_student_code ){
             	system("cls");
             	system("color 06");
@@ -189,7 +253,7 @@ int main(){
 
             }
 		}
-    	case 7: {
+    	case 8: {
     		print_list_step1( student );
     		getch();
 			break;
@@ -207,7 +271,6 @@ int main(){
     }
     }
 	
-   
 }
 void add_students_step2( SV &sv){
 	printf("Hay Nhap Ho Lot Cua Sinh Vien:"); fflush(stdin);
@@ -216,8 +279,10 @@ void add_students_step2( SV &sv){
     gets(sv.name.last_name);
     printf("\nHay Nhap Gioi Tinh Cua Sinh Vien:"); gets(sv.gender);
     printf("\nHay Nhap Ngay Sinh Cua Sinh Vien:"); gets(sv.Date_of_birth);
+    puts(sv.gender);
     printf("\nHay Nhap Dia Chi Cua Sinh Vien:");   gets(sv.address);
     printf("\n\n\n");
+  
 }
 void add_students_step1( SV a[] , int students_need_add ){
     for(int i=0;i<students_need_add;i++){
@@ -227,29 +292,101 @@ void add_students_step1( SV a[] , int students_need_add ){
         system("cls");
 
     }
+
 }
+void Filter_the_data_name(SV &sv,char name[]){
+    int key1=0,key2=0;
+    for(int i=strlen(name)-1;i>=0;i--){
+        if(name[i]!=' '&&key1==0){
+            key1=i;
+        
+        }
+        if(name[i]==' '&&key2==0){
+            key2=i;
+        }
+    }
+    int j=0;
+    strncpy(sv.name.first_name,name,key2+1);
+    for(int i=key2+1;i<=key1;i++){
+        sv.name.first_name[j]=name[i];
+        j++;
+    }
+
+
+}
+void Filter_the_data_address(SV &sv,char address[]){
+    int key;
+    for(int i=strlen(address)-1;i>=0;i--){
+       if(address[i]!=' '){
+        key=i;
+        break;
+       }
+    }
+    strncpy(sv.address,address,key+1);
+
+}
+void Read_information(SV &sv){
+    char name[35];
+    char address[50];
+    fgets(name,34,p);
+    puts(name);
+    printf("%d",strlen(name));
+    fgets(sv.gender,4,p);
+    puts(sv.gender);
+    printf("%d",strlen(sv.gender));
+    fgets(sv.Date_of_birth,11,p);
+    puts(sv.Date_of_birth);
+    printf("%d",strlen(sv.Date_of_birth));
+    fgets(address,50,p);
+    //Filter_the_data_name(sv,name);
+    //Filter_the_data_address(sv,address);
+    getch();
+}
+void Read_in_file(SV student[]){
+    p=fopen("input.txt","r");
+    if(p==NULL) {
+	       printf("\nFile Khoong Ton Tai!!!\n");
+	   }
+    else {
+        printf("\nDang Doc File!!!\n");
+        int n; // total_number_of_students_need_add
+        fscanf(p,"%d\n",&n);
+        for(int i=0; i<n; i++){
+            Read_information(student[total_number_of_students]);
+            fscanf(p,"\n");
+            total_number_of_students++; 
+        }
+        printf("\nDa Doc Xong File!!!\n");
+        fclose(p);
+    }
+} 
 void print_list_step1(SV student[]){
-    printf(" _______________________________________________________________________________________________________________________\n");
-    printf("| STT |        Ho Va Ten         | Gioi Tinh | Ngay Sinh |      Dia Chi     | Ma Sinh Vien |            Email           |\n");
+    printf(" _______________________________________________________________________________________________________________________________________________\n");
+    printf("| STT |             Ho Va Ten           |Gioi Tinh|  Ngay Sinh |                      Dia Chi                     |Ma Sinh Vien|      Email     |\n");
     for(int i = 0 ; i < total_number_of_students ; i++ ){
-      printf("| %3d |", i );
+          printf("| %3d |", i );
       print_list_step2( student[i] );
     }
-    printf(" -----------------------------------------------------------------------------------------------------------------------");
+    printf(" -----------------------------------------------------------------------------------------------------------------------------------------------");
 }
 void print_list_step2( SV sv ){
-    printf("%15s %-10s|", sv.name.first_name , sv.name.last_name );
-    printf(" %-10s|", sv.gender );
-    printf(" %-10s|", sv.Date_of_birth );
-    printf(" %-17s|", sv.address );
-    printf(" %-13s|", sv.student_code );
-    printf(" %-27s|", sv.email );
+    char name[34];
+    char b[2] = {' '};
+    strcpy( name , sv.name.first_name );
+    strcat( name , b );
+    strcat( name ,sv.name.last_name );
+    printf("%-33s|", name );
+    printf("%-9s|", sv.gender );
+    printf("%-12s|", sv.Date_of_birth );
+    printf("%-50s|", sv.address );
+    printf("%-12s|", sv.student_code ); 
+    printf("%-16s|", sv.email );
     printf("\n");
 }
 bool check_data( char data[] ){
     if( data[0] >= '0' && data[0] <= '9' ) return true;
     return false;
-}
+}   
 void delete_student( SV student[] ){
     find_student( student );
     if( check_find ){
@@ -495,20 +632,23 @@ void output_file(SV student[]){
     fclose(p);
     fp=fopen(filename,"w");
     fprintf(fp,"%-4s%d\n\n","LOP:",n+1); 
-    fprintf(fp,"%-121s\n"," _________________________________________________________________________________________________________________________");
-    fprintf(fp,"%-121s\n","| STT |         Ho Va Ten          | Gioi Tinh | Ngay Sinh |      Dia Chi     | Ma Sinh Vien |            Email           |");
+    fprintf(fp,"%-145s\n"," ______________________________________________________________________________________________________________________________________________");
+    fprintf(fp,"%-145s\n","| STT |             Ho Va Ten           |Gioi Tinh| Ngay  Sinh |                       Dia Chi                    |Ma Sinh Vien|    Email      |");
     for(int i=0;i<total_number_of_students;i++){
-    	     fprintf(fp,"%c",'|');
-             fprintf(fp,"%2s%-3d","  ",i);
-             fprintf(fp,"%c",'|');
-             fprintf(fp,"%15s%c%-10s%s",student[i].name.first_name,' ',student[i].name.last_name,"  |");
-             fprintf(fp,"%3s%-8s%c","   ",student[i].gender,'|');
-             fprintf(fp,"%3s%-8s%c","   ",student[i].Date_of_birth,'|');
-             fprintf(fp,"%5s%-13s%c","     ",student[i].address,'|');
-             fprintf(fp,"%3s%-11s%c","   ",student[i].student_code,'|');
-             fprintf(fp,"%5s%-23s%c\n","     ",student[i].email,'|');
+             fprintf(fp,"%c%2s%-3d%c",'|',"  ",i,'|');
+             char name[34];
+		     char b[2] = {' '};
+		     strcpy( name , student[i].name.first_name );
+		     strcat( name , b );
+		     strcat( name ,student[i].name.last_name );
+             fprintf(fp,"%-33s%c",name,'|');
+             fprintf(fp,"%-9s%c",student[i].gender,'|');
+             fprintf(fp,"%-12s%c",student[i].Date_of_birth,'|');
+             fprintf(fp,"%-50s%c",student[i].address,'|');
+             fprintf(fp,"%-12s%c",student[i].student_code,'|');
+             fprintf(fp,"%-15s%c\n",student[i].email,'|');
         }
-    fprintf(fp,"%-121s"," -----------------------------------------------------------------------------------------------------------------------");    
+    fprintf(fp,"%-145s"," ----------------------------------------------------------------------------------------------------------------------------------------------");    
     fclose(fp);
 }
 /* Do An Quan Li Hoc Sinh */
